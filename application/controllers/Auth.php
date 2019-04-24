046e0996e00f155409815ec1d08f56a12a5346ff
PassionZale <372470745@qq.com>
2019-04-24 19:45:52 +0800

    1

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -1,8 +1,8 @@
 <?php
 
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined("BASEPATH") OR exit("No direct script access allowed");
 
-require_once APPPATH . 'libraries/REST_Controller.php';
+require_once APPPATH . "libraries/REST_Controller.php";
 
 use Restserver\Libraries\REST_Controller;
 
@@ -10,27 +10,28 @@ class Auth extends REST_Controller {
 
     function __construct() {
         parent::__construct();
-        $this->load->library('authorization');
+        $this->load->library("authorization");
+        $this->load->model("AuthUser");
     }
 
     public function login_post() {
-        $username = $this->post('username');
-        $password = $this->post('password');
+        $username = $this->post("username");
+        $password = $this->post("password");
 
-        $query = $this->db->select('id, password, is_active')->where('username', $username)->get("auth_user");
-        $user = $query->row_array();
+        $condition = array("username" => $username);
+        $user = $this->AuthUser->show($condition);
 
         if ($user) {
-            if ($user['is_active'] == 1) {
-                $verify = password_verify($password, $user['password']);
+            if ($user["is_active"] == 1) {
+                $verify = password_verify($password, $user["password"]);
                 if ($verify) {
                     $data = array(
-                        'userId' => $user['id'],
+                        "userId" => $user["id"],
                     );
                     $jwt = $this->authorization->generateToken($data);
                     return $this->response(array(
-                                'data' => $jwt,
-                                'message' => '登录成功'
+                                "data" => $jwt,
+                                "message" => "登录成功"
                                     ), 200);
                 } else {
                     return $this->response(["message" => "账户或密码错误"], 401);
@@ -45,7 +46,7 @@ class Auth extends REST_Controller {
     public function user_get() {
         $jwt = $this->head("Authorization");
         $data = $this->authorization->validateToken($jwt);
-        $this->response(['data' => $data]);
+        $this->response(["data" => $data]);
     }
 
 }
--- application/models/AuthUser.php
+++ application/models/AuthUser.php
@@ -5,6 +5,16 @@ class AuthUser extends CI_Model {
     public function __construct() {
         parent::__construct();
     }
+    
+    public function all($condition = array()) {
+        $query = $this->db->where($condition)->get("auth_user");
+        return $query->result_array();
+    }
+
+    public function show($condition = array()) {
+        $query = $this->db->where($condition)->get("auth_user");
+        return $query->row_array();
+    }
 
     public function create($data, $is_superuser = FALSE) {
         $options = ['cost' => 8];
