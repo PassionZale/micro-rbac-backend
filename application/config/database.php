c744c26b710b8836fcbe5eec4a935b7c16cc955e
PassionZale <372470745@qq.com>
2019-06-11 16:23:28 +0800

    数据库连接驱动更换为 pdo

--- application/config/database.php
+++ application/config/database.php
@@ -74,12 +74,12 @@ $active_group = 'default';
 $query_builder = TRUE;
 
 $db['default'] = array(
-	'dsn'	=> '',
-	'hostname' => '127.0.0.1',
+	'dsn'	=> 'mysql:host=127.0.0.1;port=3306;dbname=rbac',
+	'hostname' => '',
 	'username' => 'root',
 	'password' => 'root',
-	'database' => 'rbac',
-	'dbdriver' => 'mysqli',
+	'database' => '',
+	'dbdriver' => 'pdo',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
 	'db_debug' => (ENVIRONMENT !== 'production'),
