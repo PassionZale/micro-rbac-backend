a0462523c78e7f73bc23c7f60d81b1831776d9ad
PassionZale <372470745@qq.com>
2019-07-02 18:15:51 +0800

    auth_permission 表初始化迁移脚本

--- application/migrations/014_Init_Permission_Table.php
+++ application/migrations/014_Init_Permission_Table.php
@@ -21,7 +21,28 @@ class Migration_Init_Permission_Table extends CI_Migration {
             array('name' => '编辑用户', 'code' => 'can update user', 'route' => 'put.user', 'created_at' => time()),
             array('name' => '删除用户', 'code' => 'can delete user', 'route' => 'delete.user', 'created_at' => time()),
             array('name' => '查询用户', 'code' => 'can select user', 'route' => 'get.user', 'created_at' => time()),
+            // Brand
+            array('name' => '创建品牌', 'code' => 'can create brand', 'route' => 'post.brand', 'created_at' => time()),
+            array('name' => '编辑品牌', 'code' => 'can update brand', 'route' => 'put.brand', 'created_at' => time()),
+            array('name' => '删除品牌', 'code' => 'can delete brand', 'route' => 'delete.brand', 'created_at' => time()),
+            array('name' => '查询品牌', 'code' => 'can select brand', 'route' => 'get.brand', 'created_at' => time()),
+            // Property
+            array('name' => '创建属性/规格', 'code' => 'can create property', 'route' => 'post.property', 'created_at' => time()),
+            array('name' => '编辑属性/规格', 'code' => 'can update property', 'route' => 'put.property', 'created_at' => time()),
+            array('name' => '删除属性/规格', 'code' => 'can delete property', 'route' => 'delete.property', 'created_at' => time()),
+            array('name' => '查询属性/规格', 'code' => 'can select property', 'route' => 'get.property', 'created_at' => time()),
+            // Category
+            array('name' => '创建分类', 'code' => 'can create category', 'route' => 'post.category', 'created_at' => time()),
+            array('name' => '编辑分类', 'code' => 'can update category', 'route' => 'put.category', 'created_at' => time()),
+            array('name' => '删除分类', 'code' => 'can delete category', 'route' => 'delete.category', 'created_at' => time()),
+            array('name' => '查询分类', 'code' => 'can select category', 'route' => 'get.category', 'created_at' => time()),
+            // Product
+            array('name' => '创建商品', 'code' => 'can create product', 'route' => 'post.product', 'created_at' => time()),
+            array('name' => '编辑商品', 'code' => 'can update product', 'route' => 'put.product', 'created_at' => time()),
+            array('name' => '删除商品', 'code' => 'can delete product', 'route' => 'delete.product', 'created_at' => time()),
+            array('name' => '查询商品', 'code' => 'can select product', 'route' => 'get.product', 'created_at' => time()),
         );
+
         $this->db->insert_batch('auth_permission', $data);
     }
 
